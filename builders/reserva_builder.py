5ec5f3718d420ecf9e407cd6a8c10bfc8c4e7baf
Cristofher2023 <134649110+Cristofher2023@users.noreply.github.com>
2025-06-02 20:22:58 -0400

    Modificacion  del  builder

--- builders/reserva_builder.py
+++ builders/reserva_builder.py
@@ -5,12 +5,20 @@ class ReservaBuilder:
     def __init__(self):
         self.reserva = {}
 
-    def set_usuario(self, usuario):
-        self.reserva["usuario"] = usuario
+    def set_usuario_id(self, usuario_id):
+        self.reserva["usuario_id"] = usuario_id
         return self
 
-    def set_sala(self, sala):
-        self.reserva["sala"] = sala
+    def set_sala_id(self, sala_id):
+        self.reserva["sala_id"] = sala_id
+        return self
+
+    def set_fecha_inicio(self, fecha_inicio):
+        self.reserva["fecha_inicio"] = fecha_inicio
+        return self
+
+    def set_fecha_fin(self, fecha_fin):
+        self.reserva["fecha_fin"] = fecha_fin
         return self
 
     def build(self):
