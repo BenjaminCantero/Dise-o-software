459940a4d59c5ab445f7653ccbd88cce4a40a151
BenjaminCantero <bcantero2023@alu.uct.cl>
2025-06-14 17:31:28 -0400

    limpieza
    aaaa

--- create_tables.py
+++
@@ -1,9 +0,0 @@
-from repositories.db import engine
-from repositories.models import Base
-
-def crear_tablas():
-    Base.metadata.create_all(bind=engine)
-    print("Tablas creadas correctamente.")
-
-if __name__ == "__main__":
-    crear_tablas()
