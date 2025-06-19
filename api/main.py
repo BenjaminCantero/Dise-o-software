f768c638a168fbdf4fbf0ec22c70e4bf942511ec
imeduuu <128259631+imeduuu@users.noreply.github.com>
2025-06-19 10:54:49 -0400

    limpieza

--- api/main.py
+++ api/main.py
@@ -3,17 +3,9 @@ from .db import Base, engine
 from .routes.reservas import router as reservas_router
 from .routes.usuarios import router as usuarios_router
 from .routes.salas import router as salas_router
-from .middleware.logging import LoggingMiddleware
-from .middleware.errores import ErrorHandlingMiddleware
-from .middleware.header import CustomHeaderMiddleware
-from .middleware.autenticacion import AuthMiddleware
 
 app = FastAPI()
 
-app.add_middleware(LoggingMiddleware)
-app.add_middleware(ErrorHandlingMiddleware)
-app.add_middleware(CustomHeaderMiddleware)
-app.add_middleware(AuthMiddleware)  
 
 Base.metadata.create_all(bind=engine)
 
